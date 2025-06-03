4649e0faedfcc49961447d97b3cc2eaf89146703
adil <adilahmeddev@gmail.com>
2025-06-03 10:39:41 +0700

    lexer: all tokens implemted

--- src/lexer.rs
+++ src/lexer.rs
@@ -42,6 +42,10 @@ impl Lexer {
         }
         let tok = match self.ch {
             '(' => Token::LParen,
+            '"' => {
+                let (start, end) = self.read_full_string();
+                Token::String(self.input[start..end].to_string())
+            }
             ')' => Token::RParen,
             '{' => Token::LBrace,
             '}' => Token::RBrace,
@@ -49,7 +53,14 @@ impl Lexer {
             '-' => Token::Minus,
             ',' => Token::Comma,
             '*' => Token::Asterisk,
-            '!' => Token::Bang,
+            '!' => {
+                let (start, end) = self.read_string('!', '=');
+                if end - start == 2 {
+                    Token::NotEq
+                } else {
+                    Token::Bang
+                }
+            }
             '/' => Token::Slash,
             '<' => Token::Lt,
             '>' => Token::Gt,
@@ -61,7 +72,14 @@ impl Lexer {
                 let (start, end) = self.read_string('0', '9');
                 Token::Int(self.input[start..end].to_string())
             }
-            '=' => Token::Eq,
+            '=' => {
+                let (start, end) = self.read_string('=', '=');
+                if end - start == 2 {
+                    Token::Eq
+                } else {
+                    Token::Assign
+                }
+            }
             'a'..='z' | 'A'..='Z' => {
                 let (start, end) = self.read_string_2('a', 'z', 'A', 'Z');
                 Token::get_keyword(&self.input[start..end])
@@ -72,6 +90,21 @@ impl Lexer {
         tok
     }
 
+    fn read_full_string(&mut self) -> (usize, usize) {
+        self.read_position = self.position;
+
+        let start = self.position + 1;
+        let mut end = self.position + 2;
+        while self.position < self.input.len()
+            && end < self.input.len()
+            && self.input.chars().collect::<Vec<char>>()[end] != '"'
+        {
+            end += 1;
+            self.position = end;
+        }
+        self.read_position = self.position;
+        (start, end)
+    }
     fn read_string_2(&mut self, a: char, b: char, c: char, d: char) -> (usize, usize) {
         self.read_position = self.position;
 
@@ -93,7 +126,7 @@ impl Lexer {
         self.read_position = self.position;
 
         let start = self.position;
-        let mut end = self.position + 1;
+        let mut end = self.position;
         while self.position < self.input.len() {
             let next = self.peek();
             if a <= next && b >= next {
@@ -111,11 +144,34 @@ impl Lexer {
 #[cfg(test)]
 pub(crate) mod tests {
     use super::*;
+    #[test]
+    fn assign() {
+        let mut lexer = Lexer {
+            input: String::from("="),
+            position: 0,
+            read_position: 0,
+            ch: '0',
+        };
+
+        assert_eq!(lexer.next_token(), Token::Assign);
+    }
+
+    #[test]
+    fn eq() {
+        let mut lexer = Lexer {
+            input: String::from("=="),
+            position: 0,
+            read_position: 0,
+            ch: '0',
+        };
+
+        assert_eq!(lexer.next_token(), Token::Eq);
+    }
 
     #[test]
     fn read_char() {
         let mut lexer = Lexer {
-            input: String::from("999+(){},;hello let Hello"),
+            input: String::from("999+ ! (){},;hello let = Hello == != \"string hello\""),
             position: 0,
             read_position: 0,
             ch: '0',
@@ -123,6 +179,7 @@ pub(crate) mod tests {
 
         assert_eq!(lexer.next_token(), Token::Int(String::from("999")));
         assert_eq!(lexer.next_token(), Token::Plus);
+        assert_eq!(lexer.next_token(), Token::Bang);
         assert_eq!(lexer.next_token(), Token::LParen);
         assert_eq!(lexer.next_token(), Token::RParen);
         assert_eq!(lexer.next_token(), Token::LBrace);
@@ -131,6 +188,13 @@ pub(crate) mod tests {
         assert_eq!(lexer.next_token(), Token::Semicolon);
         assert_eq!(lexer.next_token(), Token::Ident("hello".to_string()));
         assert_eq!(lexer.next_token(), Token::Let);
+        assert_eq!(lexer.next_token(), Token::Assign);
         assert_eq!(lexer.next_token(), Token::Ident("Hello".to_string()));
+        assert_eq!(lexer.next_token(), Token::Eq);
+        assert_eq!(lexer.next_token(), Token::NotEq);
+        assert_eq!(
+            lexer.next_token(),
+            Token::String("string hello".to_string())
+        );
     }
 }
